1c419bb2218ea7713158a59c8e3e6b91abcc942e
xxqyjk <xxqyjk@163.com>
2018-06-10 19:24:55 +0800

    返回数据库中的名字到html网页

--- message/views.py
+++ message/views.py
@@ -7,28 +7,11 @@ from .models import UserMessage
 # Create your views here.
 
 def getform(request):
-    # all_messages = UserMessage.objects.all()
-    all_messages = UserMessage.objects.filter(name="bobbytest")
+    message = None
+    all_messages = UserMessage.objects.filter(name="bobby")
     if all_messages:
         message = all_messages[0]
 
-    # all_messages = UserMessage.objects.filter(name="bobby", address='北京')
-    # all_messages.delete()
-    for message in all_messages:
-        message.delete()
-        # print message.name
-
-    # if request.method == 'POST':
-    #     name = request.POST.get('name','')
-    #     message = request.POST.get('message', '')
-    #     email = request.POST.get('email', '')
-    #     address = request.POST.get('address', '')
-    #     user_message = UserMessage()
-    #     user_message.name = name
-    #     user_message.message = message
-    #     user_message.address = address
-    #     user_message.email = email
-    #     user_message.object_id = "helloworld3"
-    #     user_message.save()
-
-    return render(request, 'message_form.html')
+    return render(request, 'message_form.html', {
+        "my_message":message
+    })
